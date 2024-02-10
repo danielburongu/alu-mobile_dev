164b7e7f76ee0d3faafb7556745ca7092dc3d3dc
Gasorekibo <m.gasore@alustudent.com>
2024-02-10 16:07:26 +0200

    add welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,102 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: HelloWorld(),
+      home: WelcomePage(),
     );
   }
 }
+// Welcome page:
 
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF32a8a2),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 180), // Add space from top
+              Text(
+                "Market-Master",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white70),
+              ),
+              SizedBox(height: 50),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'lib/images/market.jpg',
+                  width: 300,
+                  height: 200,
+                  fit: BoxFit.cover,
+                
+                  // Adjust the width and height as needed
+                ),
+              ),
+              // Add space between image and buttons
+              SizedBox(height: 30),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupScreen()));
+                    },
+                    child: Text("Sign Up"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Color(0xFF32a8a2), // Change the text color to match background
+                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20), // Add space between buttons
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
+                    },
+                    child: Text("Already have an account"),
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.white,
+                      side: BorderSide(color: Colors.white),
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF32a8a2),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,size: 40.0, color: Color.fromARGB(255, 255, 255, 255)),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person,size: 40.0, color: Color.fromARGB(255, 255, 255, 255)),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add,size: 40.0, color: Color.fromARGB(255, 255, 255, 255)),
+            label: 'Add Product',
+          ),
+        ],
+      ),
+    );
+  }
+}
 class HelloWorld extends StatelessWidget {
   const HelloWorld({super.key});
 
