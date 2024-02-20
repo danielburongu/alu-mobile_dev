b1bf58921a5f36d9caf13265fd62c151d42dc692
Fabrice <nkurunziz@alustudent.com>
2024-02-20 13:30:56 +0200

    removed one product from the list of products

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: AddProductScreen(),
+      home: WelcomePage(),
     );
   }
 }
