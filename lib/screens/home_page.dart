c7ed8d58048a54b6155c08155aa6015f4774e8e4
danielburongu <d.burongu@alustudent.com>
2024-02-27 17:23:28 +0300

    products update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -18,21 +18,21 @@ class _GridBState extends State<HomeScreen> {
   final List<Map<String, dynamic>> gridMap = [
     {
       "name": "Jeans",
-      "title": "Black Jeans with blue stripes",
+      "title": "Black Jeans with blue stripes for a perfect gent",
       "price": "\$10",
       "images":
           "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     },
     {
       "name": "Air Force",
-      "title": "A white air force shoes",
+      "title": "A white air force shoes for your comfort ",
       "price": "\$20",
       "images":
           "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     },
     {
       "name": "Nike Sport shoes",
-      "title": "Red shoes with black stripes",
+      "title": "Red shoes with black stripes ready for a rock star",
       "price": "\$18",
       "images":
           "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
@@ -40,7 +40,7 @@ class _GridBState extends State<HomeScreen> {
 
     {
       "name": "T-Shirt",
-      "title": "Alpha t-shirt for alpha testers.",
+      "title": "Alpha t-shirt for alpha testers(Amazing fashion).",
       "price": "\$25",
       "images":
           "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
@@ -55,12 +55,54 @@ class _GridBState extends State<HomeScreen> {
 
     {
       "name": "Pork",
-      "title": "1kg of Fresh meat(pig)",
+      "title": "1kg of Fresh meat(pig) from the land of thousand hills",
       "price": "\$3.5",
       "images":
           "https://media.post.rvohealth.io/wp-content/uploads/2020/08/pork-1200x628-facebook-1200x628.jpg",
     },
-    // Add more product data as needed
+    // Add more product data as needed_daniel
+    {
+      "name": "Vegetables",
+      "title": "Fresh assorted vegetables",
+      "price": "\$3",
+      "images":
+      "https://unsplash.com/photos/a-pile-of-different-types-of-vegetables-on-a-white-surface-5aJVJvJ9rG8",
+    },
+    {
+      "name": "Meat",
+      "title": "Fresh meat",
+      "price": "\$4",
+      "images":
+      "https://unsplash.com/photos/sliced-raw-meat-with-green-leaves-cvj-7TZjvA0",
+    },
+    {
+      "name": "Sombe",
+      "title": "Traditional African dish",
+      "price": "\$1",
+      "images":
+      "https://unsplash.com/photos/a-close-up-of-a-tree-jp5bf3RmhWI",
+    },
+    {
+      "name": "Rice",
+      "title": "Fresh rice from the beautiful farm lands across the country",
+      "price": "\$1.2",
+      "images":
+      "https://unsplash.com/photos/white-rice-grains-on-brown-wooden-table--LdilhDx3sk",
+    },
+    {
+      "name": "Cassava",
+      "title": "Fresh cassava staple African food",
+      "price": "\$1",
+      "images":
+      "https://unsplash.com/photos/a-pile-of-wood-vP5guwApg0g",
+    },
+    {
+      "name": "Onions",
+      "title": "Fresh onions",
+      "price": "\$0.5",
+      "images":
+      "https://unsplash.com/photos/red-onion-on-brown-wooden-crate-SSIwIRCu7bM",
+    },
   ];
 
   List<String> docIds = [];
